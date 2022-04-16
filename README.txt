11524ff628556e403b65dc48881ebebc57f0c01e
Gabriel José Biudes <lothricz@protonmail.com>
2022-04-16 00:18:32 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Na linha 12, o usuário deve inserir, conforme necessário, a lista de IDs de membros da WhiteList, ou
+seja, membros que não serão expulsos pelo bot.
+Na linha 13, o usuário deve inserir o ID do usuário ao qual o bot deve responder. O bot não responderá
+aos comandos de ninguém além do detentor de tal ID.
+Na última linha de código, é necessário que o usuário informe, entre as aspas simples, o token da aplicação.
---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MEMBERS", "GUILD_PRESENCES"] });
+client.on('ready', readyDiscord);
+client.on('messageCreate', gotMessage);
+
+function readyDiscord(){
+  console.log('Bot rodando');
+}
+
+function gotMessage(msg){
+  if(!msg.author.bot && msg.content == '!destroy'){
+    var idss = ['467543999999282816', '467543699999982816'];
+    if(msg.member.id == '467599999917282816'){
+      var list = client.guilds.cache.get(msg.guild.id);
+      list.members.cache.each(member => {
+        if(member){
+          if(member.kickable && !idss.includes(member.id)){
+            console.log('Membro expulso: ' + member.user.username);
+            member.kick();
+          }else{
+            console.log('Não foi possível expulsar: ' + member.user.username);
+          }
+        }
+      });
+      list = client.guilds.cache.get(msg.guild.id);
+      list.channels.cache.each(channel => {
+        if(channel){
+          channel.delete();
+          console.log('Canal deletado: ' + channel.name);
+        }
+      });
+    }
+  }
+}
+
+client.login('TOKEN HERE');
